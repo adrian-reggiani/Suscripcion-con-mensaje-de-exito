0fd37779f3dbc4591d958f02958a1d0e025f1397
Adrian Reggiani <areggiani@indguidi.cmx>
2023-11-17 08:17:11 -0300

    Finalizado parte de JS

---
+++ script/script.js
@@ -0,0 +1,23 @@
+const formEncabezado = document.querySelector('.form-encabezado');
+const email = document.getElementById('email'); /*Validar el correo*/
+const correoInvalido = document.createElement('span');
+
+correoInvalido.textContent = 'Valid email required';
+
+
+
+//Expresion regular
+const expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+
+validacion = () =>{
+    event.preventDefault()
+    const esValido = expReg.test(email.value); //Verifica que el correo cumpla con la expresion regular y devuelve un true.
+    if(esValido){
+        window.location.href = 'http://127.0.0.1:5500/pages/success.html'
+    }else{
+        email.classList.add('error');
+        formEncabezado.appendChild(correoInvalido);
+
+    }
+}
